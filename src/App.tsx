8e2dfae709263ba5d20dfce0085bbf3c4aaa8b9d
xiongfeng <343138759@qq.com>
2022-05-02 09:40:41 +0800

    更新配置

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import React from "react";
 // import "./App.css";
 
 // import ProjectScreen from "screens/project-list";
-import ReactList from "./screens/practice/react-list";
+// import ReactList from "./screens/practice/react-list";
+import LoginScreen from "screens/login";
 function App() {
-  return <ReactList />;
+  return <LoginScreen />;
 }
 
 export default App;
