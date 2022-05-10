9f558b47a1a6d078070e1423d13ceb995bca5925
xiongfeng <343138759@qq.com>
2022-05-10 13:53:46 +0800

    处理警告

--- src/screens/project-list/project-popover.tsx
+++ src/screens/project-list/project-popover.tsx
@@ -2,7 +2,7 @@
  * @Author: xiongfeng '343138759@qq.com'
  * @Date: 2022-05-08 16:58:54
  * @LastEditors: xiongfeng '343138759@qq.com'
- * @LastEditTime: 2022-05-10 10:15:21
+ * @LastEditTime: 2022-05-10 11:12:33
  * @FilePath: \Typescript练习d:\王者农药plus\web前端\慕课网react项目\jira\src\screens\project-list\project-popover.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,7 +20,7 @@ export default function ProjectPopover(props: { open: () => void }) {
       <Typography.Text type="secondary">收藏项目</Typography.Text>
       <List>
         {pinnedProjects?.map((project) => (
-          <List.Item>
+          <List.Item key={project.id}>
             <List.Item.Meta title={project.name} />
           </List.Item>
         ))}
