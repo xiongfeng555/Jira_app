0cf1dcd44d77060d314b830a66879dc17a1f5cd0
xiongfeng <343138759@qq.com>
2022-05-11 15:08:06 +0800

    简化代码

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,12 +2,11 @@
  * @Author: xiongfeng '343138759@qq.com'
  * @Date: 2022-04-28 19:58:28
  * @LastEditors: xiongfeng '343138759@qq.com'
- * @LastEditTime: 2022-05-11 14:58:19
+ * @LastEditTime: 2022-05-11 15:07:00
  * @FilePath: \Typescript练习d:\王者农药plus\web前端\慕课网react项目\jira\src\screens\project-list\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React, { useState, useEffect } from "react";
-import { useDebounce } from "utils";
 import List from "./List";
 import Search from "./Search";
 import { useHttp } from "utils/http";
@@ -26,28 +25,9 @@ export default function ProjectListScreen(props: projectModalProps) {
   const client = useHttp();
   const questLists = useProjects();
   useEffect(() => {
-    // fetch(
-    //   `${baseURL}/projects?${qs.stringify(cleanObject(debouncedParams))}`
-    // ).then(async (response) => {
-    //   setProjects(await response.json());
-    // });
     run(questLists());
-
-    // client("projects", {
-    //   data: cleanObject(debouncedParams),
-    // })
-    //   .then((data) => setProjects(data))
-    //   .catch((error) => {
-    //     setProjects([]);
-    //     setError(error);
-    //   })
-    //   .finally(() => setLoading(false));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [questLists, run]);
   useEffect(() => {
-    // fetch(`${baseURL}/users`).then(async (response) => {
-    //   setUsers(await response.json());
-    // });
     client("users").then((data) => setUsers(data));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
