f60306a2eec6352ef808ee81145450278eb65939
xiongfeng <343138759@qq.com>
2022-05-11 14:59:14 +0800

    添加redux-devtool

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,10 +2,11 @@
  * @Author: xiongfeng '343138759@qq.com'
  * @Date: 2022-05-10 09:21:34
  * @LastEditors: xiongfeng '343138759@qq.com'
- * @LastEditTime: 2022-05-10 09:30:45
+ * @LastEditTime: 2022-05-11 14:41:35
  * @FilePath: \Typescript练习d:\王者农药plus\web前端\慕课网react项目\jira\src\redux\store.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { createStore } from "redux";
 import { allReducer } from "./reducer";
-export default createStore(allReducer);
+import { composeWithDevTools } from "redux-devtools-extension";
+export default createStore(allReducer, composeWithDevTools());
