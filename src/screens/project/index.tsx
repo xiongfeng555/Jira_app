f5628f07947c82b0fd144ac5c3554ff034870fc0
xiongfeng <343138759@qq.com>
2022-05-12 10:20:54 +0800

    解决看板页面路由后退无效问题

--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: xiongfeng '343138759@qq.com'
+ * @Date: 2022-05-06 10:14:06
+ * @LastEditors: xiongfeng '343138759@qq.com'
+ * @LastEditTime: 2022-05-12 10:20:00
+ * @FilePath: \Typescript练习d:\王者农药plus\web前端\慕课网react项目\jira\src\screens\project\index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from "react";
 import { Link, Routes, Route, Navigate } from "react-router-dom";
 import EpicScreen from "screens/epic";
@@ -11,7 +19,10 @@ export default function ProjectScreen() {
       <Routes>
         <Route path="/kanpan" element={<KanPanScreen />}></Route>
         <Route path="/epic" element={<EpicScreen />}></Route>
-        <Route path="/" element={<Navigate to={"kanpan"} />}></Route>
+        <Route
+          path="/"
+          element={<Navigate to={"kanpan"} replace={true} />}
+        ></Route>
       </Routes>
     </div>
   );
