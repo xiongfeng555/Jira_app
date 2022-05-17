9e4b587e0e14cbb4d186b31cbd607356946e6a59
xiongfeng <343138759@qq.com>
2022-05-17 16:01:38 +0800

    完成删除和搜索高亮

--- src/screens/kanban/kanban-colunm.tsx
+++ src/screens/kanban/kanban-colunm.tsx
@@ -2,7 +2,7 @@
  * @Author: xiongfeng '343138759@qq.com'
  * @Date: 2022-05-12 14:55:18
  * @LastEditors: xiongfeng '343138759@qq.com'
- * @LastEditTime: 2022-05-17 14:57:28
+ * @LastEditTime: 2022-05-17 16:00:51
  * @FilePath: \Typescript练习d:\王者农药plus\web前端\慕课网react项目\jira\src\screens\kanban\kanban-colunm.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,9 +14,16 @@ import taskIcon from "../../assets/task.svg";
 import bugIcon from "../../assets/bug.svg";
 import { useTaskTypes } from "utils/task-type";
 import styled from "@emotion/styled";
-import { Card } from "antd";
-import { useTasksModal, useTasksSearchParams } from "./util";
+import { Button, Card, Dropdown, Menu, Modal } from "antd";
+import {
+  useKanbansQueryKey,
+  useTasksModal,
+  useTasksSearchParams,
+} from "./util";
 import CreateTask from "./create-task";
+import Mark from "components/mark";
+import { useDeleteKanban } from "utils/use-kanban";
+import { Row } from "components/lib";
 
 const TaskTypeIcon = ({ id }: { id: number }) => {
   const { data: taskTypes } = useTaskTypes();
@@ -33,7 +40,20 @@ const TaskTypeIcon = ({ id }: { id: number }) => {
     />
   );
 };
-
+const TaskCard = ({ task }: { task: Task }) => {
+  const { startEdit } = useTasksModal();
+  const { name: keyword } = useTasksSearchParams();
+  return (
+    <Card
+      onClick={() => startEdit(task.id)}
+      key={task.id}
+      style={{ marginBottom: "0.5rem", cursor: "pointer" }}
+    >
+      <Mark keyword={keyword} name={task.name} />
+      <TaskTypeIcon id={task.typeId} />
+    </Card>
+  );
+};
 export default function KanbanColunm({
   kanban,
   key,
@@ -43,28 +63,51 @@ export default function KanbanColunm({
 }) {
   const { data: allTasks } = useTasks(useTasksSearchParams());
   const tasks = allTasks?.filter((task: Task) => task.kanbanId === kanban.id);
-  const { startEdit } = useTasksModal();
+
   return (
     <Container>
-      <h3 style={{ fontWeight: "bolder" }}>{kanban.name}</h3>
+      <Row between={true}>
+        <h3 style={{ fontWeight: "bolder" }}>{kanban.name}</h3>
+        <More kanban={kanban} />
+      </Row>
+
       <TasksContainer>
         {tasks?.map((task: any) => {
-          return (
-            <Card
-              onClick={() => startEdit(task.id)}
-              key={task.id}
-              style={{ marginBottom: "0.5rem", cursor: "pointer" }}
-            >
-              {task.name}
-              <TaskTypeIcon id={task.typeId} />
-            </Card>
-          );
+          return <TaskCard task={task} />;
         })}
         <CreateTask kanbanId={kanban.id} />
       </TasksContainer>
     </Container>
   );
 }
+const More = ({ kanban }: { kanban: Kanban }) => {
+  const { mutateAsync } = useDeleteKanban(useKanbansQueryKey());
+  const startEdit = () => {
+    Modal.confirm({
+      okText: "确定",
+      cancelText: "取消",
+      title: "确定删除看板吗？",
+      onOk() {
+        return mutateAsync(Number(kanban.id));
+      },
+    });
+  };
+  const overlay = (
+    <Menu>
+      <Menu.Item>
+        <Button type="link" onClick={startEdit}>
+          删除
+        </Button>
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Dropdown overlay={overlay}>
+      <Button type="link">...</Button>
+    </Dropdown>
+  );
+};
 export const Container = styled.div`
   min-width: 27rem;
   border-radius: 6px;
--- src/screens/kanban/util.ts
+++ src/screens/kanban/util.ts
@@ -2,7 +2,7 @@
  * @Author: xiongfeng '343138759@qq.com'
  * @Date: 2022-05-12 14:42:06
  * @LastEditors: xiongfeng '343138759@qq.com'
- * @LastEditTime: 2022-05-17 15:00:19
+ * @LastEditTime: 2022-05-17 15:42:18
  * @FilePath: \Typescript练习d:\王者农药plus\web前端\慕课网react项目\jira\src\screens\kanban\util.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
