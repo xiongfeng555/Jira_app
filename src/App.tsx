fca3b0d334a949da6f830cb0eda1e906012a42ae
xiongfeng <343138759@qq.com>
2022-05-18 13:50:00 +0800

    使用lazy优化性能

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { lazy } from "react";
 
 import "./App.css";
 import ErrorBoudary from "views/error-boudary";
 // import ProjectScreen from "screens/project-list";
 // import ReactList from "./screens/practice/react-list";
-import AuthenticatedApp from "views/authenticated-app";
-import UnAuthenticatedApp from "views/unauthenticated-app";
+// import AuthenticatedApp from "views/authenticated-app";
+// import UnAuthenticatedApp from "views/unauthenticated-app";
 import { FullPageErrorFallback } from "./components/lib";
 import { useAuth } from "context/auth-context";
+const AuthenticatedApp = lazy(() => import("views/authenticated-app"));
+const UnAuthenticatedApp = lazy(() => import("views/unauthenticated-app"));
 function App() {
   const { user } = useAuth();
   return (
